1495e5c43326525b6102d8b1b040506edd769439
jeremyhu134 <52206356+jeremyhu134@users.noreply.github.com>
2020-11-10 19:46:11 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,33 +1,25 @@
 const config = {
-    type: Phaser.AUTO,
-    width : 800,
-    height: 550,
-    backgroundColor: "#000000",
-    audio: {
-        disableWebAudio: true
-      },
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: "000000",
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 1000 },
-            enableBody: true
-            //debug: true
+          gravity: { y: 0 },
+          enableBody: true,
         }
     },
-    scene:[MenuScene,IntroScene,Mission2Scene,Mission3Scene,DandVScene],
+  scene:[MenuScene,introNight,SIXAM,camera,Night1,gameOver],
     scale: {
-        zoom: 1.3
+        zoom: 0.95
     }
 };
 
 const game = new Phaser.Game(config);
 
-let gameState = {
-    missionnumber : 1,
-    loadSave:()=>{
-        if(typeof localStorage.missionnumber !== 'number'){
-            gameState.missionnumber = 1;
-        }
-        gameState.missionnumber = localStorage.missionnumber;
-    }
+let gameState = {               
+    night: 1,
+    ldoorOpen: true,
+    rdoorOpen: true
 }
